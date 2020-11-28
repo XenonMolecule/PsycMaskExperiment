1a95f77c0d8363b4a85c891ef452f420220aa664
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-11-27 22:45:15 -0500

    Added simple home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
-import ClientComponent from "./ClientComponent";
+
+import Home from './pages/Home.js';
 
 function App() {
-    const [loadClient, setLoadClient] = useState(true);
     return (
         <>
-            {/* LOAD OR UNLOAD THE CLIENT */}
-            <button onClick={() => setLoadClient(prevState => !prevState)}>
-                TOGGLE CLIENT
-            </button>
-            {/* SOCKET IO CLIENT*/}
-            {loadClient ? <ClientComponent /> : null}
+            <Home/>
         </>
     );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+
+function Home() {
+    return (
+        <Jumbotron style={{"textAlign":"center"}}>
+            <h1>PSYC 1101 Masks and Emotions Experiment</h1>
+            <p>
+                Thanks for taking the time to check out our website for our final project for PSYC1101!
+            </p>
+            <p> If you're interested in participating in the study you can click "Get started" below, and participate
+                for as long as you want.  There is no time commitment - we continuously collect responses, so you can
+                participate for as few as 30 seconds to as long as 10 minutes, 20 minutes,
+                or even 20 years - we're not judging.
+            </p>
+            <p>
+                If you're interested in learning more about this project you can also click "Learn more"
+            </p>
+            <p>
+                <Button variant="primary" size="lg">Get started</Button>
+            </p>
+            <p>
+                <Button variant="link" size="lg">Learn more</Button>
+            </p>
+        </Jumbotron>
+    );
+}
+
+export default Home;
