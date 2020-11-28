2bf79ea65d881b0838bcc3e35ca7ddc3f700f85a
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-11-28 14:00:04 -0500

    Added images

--- data/collect-images.py
+++ data/collect-images.py
@@ -5,17 +5,10 @@ from io import BytesIO
 import numpy as np
 import time
 from pynput import keyboard
-import psutil
-import keras
-from keras.models import load_model
-from keras.preprocessing import image
 
 
 START = 0
 END = -1
-
-model = load_model("./model_v6_23.hdf5")
-
 
 def download_faces(start=START, end=END):
     print("Type (s) to save and (d) to delete and (q) to quit.  Always type enter afterwards.")
@@ -29,30 +22,25 @@ def download_faces(start=START, end=END):
             np_row = np.array(row[0:15])
             np_row = np_row.reshape((3,5))
             for data in np_row:
-                response = requests.get(data[0])
-                img = Image.open(BytesIO(response.content))
-                img = img.crop((float(data[1]) * img.width,
-                            float(data[3]) * img.height,
-                            float(data[2]) * img.width,
-                            float(data[4]) * img.height))
-                print("Predicting emotion")
-                time.sleep(0.01)
-                predicted_class = np.argmax(model.predict(np.array(img)))
-                print("DONE PREDICTING")
-                print(predicted_class)
-                if predicted_class != 3:
-                    print(predicted_class)
-                    break
-                img.show()
-                with keyboard.Events() as events:
-                    for event in events:
-                        if event.key == keyboard.Key.esc or event.key.char == 'q':
-                            return
-                        if event.key.char == 's':
-                            print("SAVED")
-                            break
-                        if event.key.char == 'd':
-                            break
+                try:
+                    response = requests.get(data[0])
+                    img = Image.open(BytesIO(response.content))
+                    img = img.crop((float(data[1]) * img.width,
+                                float(data[3]) * img.height,
+                                float(data[2]) * img.width,
+                                float(data[4]) * img.height))
+                    img.show()
+                    with keyboard.Events() as events:
+                        for event in events:
+                            if event.key == keyboard.Key.esc or event.key.char == 'q':
+                                return
+                            if event.key.char == 's':
+                                print("SAVED")
+                                break
+                            if event.key.char == 'd':
+                                break
+                except:
+                    continue
                 img.close()
 
 download_faces()
